ef886ed579ebf0b14fdd0c2ab136d97b3aaf7401
Karan <162827087+Karanv777@users.noreply.github.com>
2024-10-01 22:12:05 +0530

    Brand Perception Project

---
+++ Brand_Perception_project.py
@@ -0,0 +1,38 @@
+import pandas as pd
+from matplotlib import pyplot as plt  
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+
+sentiment = SentimentIntensityAnalyzer()
+
+def analyze(text):
+    try:
+        sent_1 = sentiment.polarity_scores(text)
+
+        if sent_1["compound"] >= 0.05:
+            print("\nThe Perception of Brand is Positive 😍\n")
+
+        elif sent_1["compound"] <= -0.05:
+            print("\nThe Perception of Brand is Negative 😡\n")
+
+        else:
+            print("\nThe Perception of Brand is Neutral 😐\n")
+
+    except Exception as e:
+        print(e)
+
+
+Text = pd.read_csv("Dataset_1.csv")
+analyze(Text)
+print(Text)
+
+
+plt.rcParams["figure.figsize"] = [13.00, 5.50]
+plt.rcParams["figure.autolayout"] = True
+columns = ["userName", "score"]
+
+Text_2 = pd.read_csv("Dataset_1.csv", usecols=columns)
+# print("Contents in csv file:\n", Text)
+plt.plot(Text.userName, Text.score)
+# plt.savefig("Sentiment_graph.png")
+plt.show()
+
